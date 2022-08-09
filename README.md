f678e18eaac9f3970af1ce6e5ea3175b4c51120d
Mykhailo Diachuk <m.diachuk@insart.com>
2022-08-09 10:36:55 +0300

    :sparkles: added initial function and project config

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # shopify-order-webhook-telegram
+
+![Contributors](https://img.shields.io/github/contributors/lawrenz1337/shopify-order-webhook-telegram?style=plastic)
+![Forks](https://img.shields.io/github/forks/lawrenz1337/shopify-order-webhook-telegram)
+![Stars](https://img.shields.io/github/stars/lawrenz1337/shopify-order-webhook-telegram)
+![Licence](https://img.shields.io/github/license/lawrenz1337/shopify-order-webhook-telegram)
+![Issues](https://img.shields.io/github/issues/lawrenz1337/shopify-order-webhook-telegram)
+
+
+### Description
+
+Welcome, in this repository you will find an example on how to properly set up a Google Cloud Function as you webhook target and send telegram message on specific events, any forks/comments/PRs are appreciated!
+
+### Instructions on setup
+
+#### Required variables in `.env`
+```
+#To verify that requests are coming from shopify, can obtain in shopify shop settings -> webhooks 
+SIGNATURE=aaaaabbbbbcccccdddddeeeee
+#Telegram bot token
+TELEGRAM_TOKEN=123456789:AAAABBBBCCCCDDDDEEEE
+#ID of a group or user to send messages to
+RECIPIENT=-123456789
+```
+
+#### Optional variables in `.env`
+```
+#Generates link to open a specific order
+SHOP_ADMIN_LINK=https://your_shopify_shop.com/admin
+#Mentions specific user in a message (can be multiple users)
+MENTIONS="@AnyTelegramUser @AnotherTelegramUser"
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const functions = require('@google-cloud/functions-framework')
+const { Telegraf } = require('telegraf')
+const { createHmac } = require('crypto')
+const { SIGNATURE, TELEGRAM_TOKEN, RECIPIENT, SHOP_ADMIN_LINK, MENTIONS } = process.env
+
+const verifyHeaders = (data, hmacHeader) => {
+  const res = createHmac('sha256', SIGNATURE)
+    .update(data, 'utf8', 'hex')
+    .digest('base64')
+  return res === hmacHeader
+}
+
+functions.http('yuvalPost', (req, res) => {
+  const { headers } = req
+
+  try {
+    const isVerified = verifyHeaders(
+      req.rawBody,
+      headers['x-shopify-hmac-sha256']
+    )
+    if (isVerified) {
+      const bot = new Telegraf(TELEGRAM_TOKEN)
+
+      if (headers['x-shopify-topic'] === 'orders/create') {
+        const order = req.body
+        const message = `
+          <b>New Order</b>
+          - <b>Order ID</b>: ${order.id}
+          - <b>Order Number</b>: ${order.order_number}
+          - <b>Total Price</b>: ${order.total_price} ${order.currency}
+          - <b>Client Phone</b>: <code>${order.phone} or ${order.customer?.phone} or ${order.customer?.default_address?.phone}</code>
+          ${SHOP_ADMIN_LINK ? `<a href="${SHOP_ADMIN_LINK}/orders/${order.id}"><i>Order Link</i></a>` : ''}
+        `
+        bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+      }
+      if (headers['x-shopify-topic'] === 'themes/publish') {
+        bot.telegram.sendMessage(
+          RECIPIENT,
+            `A new theme version has been published ${MENTIONS}`
+        )
+      }
+    }
+  } catch (_) {}
+
+  res.send('OK')
+})
