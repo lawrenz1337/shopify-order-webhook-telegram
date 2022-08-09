362dc823f10d5460d57a2d940fffa1951fc13b03
Mykhailo Diachuk <m.diachuk@insart.com>
2022-08-09 12:32:35 +0300

    :recycle: refactored topics logic

--- index.js
+++ index.js
@@ -5,25 +5,18 @@ const { createHmac } = require('crypto')
 const { SIGNATURE, TELEGRAM_TOKEN, RECIPIENT, SHOP_ADMIN_LINK, MENTIONS } = process.env
 
 const verifyHeaders = (data, hmacHeader) => {
-  const res = createHmac('sha256', SIGNATURE)
+  if (!data || !hmacHeader) {
+    return false
+  }
+  const hash = createHmac('sha256', SIGNATURE)
     .update(data, 'utf8', 'hex')
     .digest('base64')
-  return res === hmacHeader
+  return hash === hmacHeader
 }
 
-functions.http('processWebhook', (req, res) => {
-  const { headers } = req
-  const bot = new Telegraf(TELEGRAM_TOKEN)
-
-  try {
-    const isVerified = verifyHeaders(
-      req.rawBody,
-      headers['x-shopify-hmac-sha256']
-    )
-    if (isVerified) {
-      if (headers['x-shopify-topic'] === 'orders/create') {
-        const order = req.body
-        const message = `
+const topics = {
+  'orders/create': ({ order, bot }) => {
+    const message = `
           <b>New Order</b>
           - <b>Order ID</b>: ${order.id}
           - <b>Order Number</b>: ${order.order_number}
@@ -31,19 +24,35 @@ functions.http('processWebhook', (req, res) => {
           - <b>Client Phone</b>: <code>${order.phone} or ${order.customer?.phone} or ${order.customer?.default_address?.phone}</code>
           ${SHOP_ADMIN_LINK ? `<a href="${SHOP_ADMIN_LINK}/orders/${order.id}"><i>Order Link</i></a>` : ''}
         `
-        bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
-      }
-      if (headers['x-shopify-topic'] === 'themes/publish') {
-        bot.telegram.sendMessage(
-          RECIPIENT,
-            `A new theme version has been published ${MENTIONS}`
-        )
-      }
-      if (headers['x-shopify-topic'] === 'themes/update') {
-        bot.telegram.sendMessage(
-          RECIPIENT,
-          'An update was made to a theme'
-        )
+    bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+  },
+  'themes/publish': ({ bot }) => {
+    bot.telegram.sendMessage(
+      RECIPIENT,
+        `A new theme version has been published ${MENTIONS}`
+    )
+  },
+  'themes/update': ({ bot }) => {
+    bot.telegram.sendMessage(
+      RECIPIENT,
+      'An update was made to a theme'
+    )
+  }
+}
+
+functions.http('processWebhook', (req, res) => {
+  const { headers, body: order, rawBody } = req
+  const bot = new Telegraf(TELEGRAM_TOKEN)
+
+  try {
+    const isVerified = verifyHeaders(
+      rawBody,
+      headers['x-shopify-hmac-sha256']
+    )
+    if (isVerified) {
+      const topic = headers['x-shopify-topic']
+      if (topics[topic]) {
+        topics[topic]({ order, bot })
       }
     }
   } catch (e) {
