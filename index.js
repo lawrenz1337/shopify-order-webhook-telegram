1466fe7fb92baa60014e10b0b1fbc6b04cd9c754
Mykhailo Diachuk <dyachukmihail@gmail.com>
2022-12-23 12:06:57 +0200

    :recycle: changed github workflow to work on push only and updated topics to not fire TG messages when cart is empty

--- index.js
+++ index.js
@@ -39,19 +39,25 @@ const topics = {
     )
   },
   'carts/update': ({ bot, order: cart }) => {
-    const message = `
-      <b>Cart ${cart.id.substring(0, 4)} has been updated</b>
-      - <b>New price</b>: ${cart.line_items.reduce((p, c) => parseFloat(p) + parseFloat(c.line_price), 0)}
-    `
-    bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+    const newPrice = cart.line_items.reduce((p, c) => parseFloat(p) + parseFloat(c.line_price), 0)
+    if (newPrice > 0) {
+      const message = `
+        <b>Cart ${cart.id.substring(0, 4)} has been updated</b>
+        - <b>New price</b>: ${newPrice}
+      `
+      bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+    } 
+    
   },
   'carts/create': ({ bot, order: cart }) => {
-    const message = `
-      <b>Cart ${cart.id.substring(0, 4)} has been created</b>
-      - <b>Number of items</b>: ${cart.line_items.length}
-      - <b>Total price</b>: ${cart.line_items.reduce((p, c) => parseFloat(p) + parseFloat(c.line_price), 0)}
-    `
-    bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+    if (cart.line_items.length > 0) {
+      const message = `
+        <b>Cart ${cart.id.substring(0, 4)} has been created</b>
+        - <b>Number of items</b>: ${cart.line_items.length}
+        - <b>Total price</b>: ${cart.line_items.reduce((p, c) => parseFloat(p) + parseFloat(c.line_price), 0)}
+      `
+      bot.telegram.sendMessage(RECIPIENT, message, { parse_mode: 'HTML' })
+    }
   }
 }
 
